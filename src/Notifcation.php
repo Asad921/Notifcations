c8bfd32826537843ca51e9d28859ac2a03f21d4b
Asad921 <officiallasad.ali@gmail.com>
2022-05-23 15:28:09 +0500

    update

--- src/Notifcation.php
+++ src/Notifcation.php
@@ -7,7 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notifcation extends Model
 {
-    
+
     use HasFactory;
+    
+   
+    protected $fillable = [
+        'type',
+        'notifiable_type ',
+        'notifiable_id ',
+        'data',
+        'read_at',
+        
+    ];
    
 }
