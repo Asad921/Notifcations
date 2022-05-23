7e76cf588c8fa8bef823ffdb127ae6422197e7d8
Asad921 <officiallasad.ali@gmail.com>
2022-05-23 14:05:36 +0500

    update

--- src/Facades/Notify.php
+++ src/Facades/Notify.php
@@ -4,6 +4,7 @@ namespace Notifcations\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Notifcations\Notifcation;
+use Auth;
 
 
 class Notify extends Facade
@@ -11,6 +12,25 @@ class Notify extends Facade
     protected static function getFacadeAccessor()
     {
         return 'Notifcations';
+    }
+    protected static function notifications($notifiable, $parent_type, $message){
+        $data = [
+            
+            'parent_id'     => Auth::user()->id,
+            'parent_type'   => $parent_type,
+            'message'       => $message,
+            'username'      =>Auth::user()->name,
+            'image'         => Auth::user()->image,
+        ];
+
+        $data = [
+            "type"              => $parent_type,
+            "notifiable_type"   => User::class,
+            "notifiable_id"     => $notifiable,
+            "data"              => serialize($data),
+        ];
+
+        Notification::create($data);
     }
     
 }
